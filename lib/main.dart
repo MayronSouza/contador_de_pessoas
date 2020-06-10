e6bdcde8a87ab5391d3489a8c2dd80864996f436
Mayron Souza <mayrondrummer@gmail.com>
2020-06-09 23:51:58 -0300

    Inserindo o Stateful no App

--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,78 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Contador de Pessoas',
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            'assets/images/restaurant.jpg',
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Contador 0',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '+1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                        ),
-                      ),
-                      onPressed: () => print('Pressionou +1'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '-1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                        ),
-                      ),
-                      onPressed: () => print('Pressionou -1'),
-                    ),
-                  )
-                ],
-              ),
-              Text(
-                'Pode Entrar!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'assets/images/restaurant.jpg',
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Contador 0',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '+1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
+                    ),
+                    onPressed: () => print('Pressionou +1'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '-1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
+                    ),
+                    onPressed: () => print('Pressionou -1'),
+                  ),
+                )
+              ],
+            ),
+            Text(
+              'Pode Entrar!',
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
